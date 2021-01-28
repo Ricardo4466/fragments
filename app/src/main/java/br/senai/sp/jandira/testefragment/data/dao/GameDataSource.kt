e2b909059520c3cf4941dcb2745cc188d6ab480a
RICARDO TEIXEIRA LIMA <20185522@CFP127ALUNO.LOCAL>
2021-01-28 16:55:46 -0300

    implementação dos recycleView

--- app/src/main/java/br/senai/sp/jandira/testefragment/data/dao/GameDataSource.kt
+++ app/src/main/java/br/senai/sp/jandira/testefragment/data/dao/GameDataSource.kt
@@ -15,29 +15,37 @@ class GameDataSource {
                 "Mortal Kombat",
                 context.getDrawable(R.drawable.mortal_kombat)!!,
                 "Mortal Kombat é muito Legal",
-                4.5,
-                listOf(
-                    Console(1,"Playstation 1", "Sony", "1996-12-01"),
-                    Console(2,"Xbox 360", "Microsoft", "1986-12-01")))
-            )
-            games.add(Game(2,
-                "Resident evil 4",
-                context.getDrawable(R.drawable.resident_evil_4)!!,
-                "Residente evil é muito Legal",
-                5.0,
+                3.0f,
                 listOf(
                     Console(1,"Playstation 1", "Sony", "1996-12-01"),
                     Console(2,"Xbox 360", "Microsoft", "1986-12-01"),
                     Console(3,"PC", "Microsoft", "1986-12-01")))
             )
+            games.add(Game(2,
+                "Resident evil 4",
+                context.getDrawable(R.drawable.resident_evil_4)!!,
+                "É o sexto jogo principal da franquia Resident Evil. ... A história de Resident Evil 4 segue o agente especial do governo dos Estados Unidos Leon S. Kennedy, que é enviado em uma missão para resgatar Ashley Graham, filha do presidente americano, que foi raptada por uma seita macabra.",
+                5.0f,
+                listOf(
+                    Console(1,"Playstation 1", "Sony", "1996-12-01"),
+                    Console(2,"Xbox 360", "Microsoft", "1986-12-01")))
+            )
             games.add(Game(3,
                 "Beyond Good&Evil",
                 context.getDrawable(R.drawable.beyond)!!,
                 "Beyond Good&Evil jogo de aventura",
-                4.7,
+                4.5f,
                 listOf(
                     Console(1,"Playstation 2", "Sony", "1996-12-01"),
                     Console(2,"Xbox 360", "Microsoft", "1986-12-01")))
+            )
+            games.add(Game(4,
+                "Counter Strike",
+                context.getDrawable(R.drawable.csgo)!!,
+                "Counter-Strike: Global Offensive é um jogo online desenvolvido pela Valve Corporation e pela Hidden Path Entertainment, sendo uma sequência de Counter-Strike: Source. É o quarto título principal da franquia.",
+                5f,
+                listOf(
+                    Console(3,"PC", "Microsoft", "1986-12-01")))
             )
 
             return games
--- app/src/main/java/br/senai/sp/jandira/testefragment/fragments/GamesFragment.kt
+++ app/src/main/java/br/senai/sp/jandira/testefragment/fragments/GamesFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.senai.sp.jandira.testefragment.R
+import br.senai.sp.jandira.testefragment.adapter.GameAdapter
+import br.senai.sp.jandira.testefragment.data.dao.GameDataSource
+import br.senai.sp.jandira.testefragment.model.Game
 
 class GamesFragment : Fragment() {
+
+    private lateinit var recyclerGames: RecyclerView
+    private val gameAdapter = GameAdapter()
+    private var gameList = listOf<Game>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +28,29 @@ class GamesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_games, container, false)
+        val view =  inflater.inflate(R.layout.fragment_games, container, false)
+
+        //Instanciar a RecyclerView
+
+        recyclerGames = view.findViewById(R.id.recycler_view_games)
+
+        //Determinar a orientação a recyclerview
+
+        recyclerGames.layoutManager = LinearLayoutManager(view.context)
+
+        // informar para recyclerview o adapter que ela irá utilizar
+
+        recyclerGames.adapter = gameAdapter
+
+        // Dizer ao adapter qual é a fonte de DADOS
+
+        gameList = GameDataSource.getGames(view.context)
+
+        // Atualizar a lista de josgos do adapter
+
+        gameAdapter.updateGameList(gameList)
+
+        return view
     }
 
 }
